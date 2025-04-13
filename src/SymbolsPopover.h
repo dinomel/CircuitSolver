1b32e659236970c63e86cb0baf3a7fc4d15aa088
dino <dinomel43@gmail.com>
2025-04-13 15:16:46 +0200

    symbol popover xml created

--- src/SymbolsPopover.h
+++ src/SymbolsPopover.h
@@ -9,21 +9,21 @@
 class SymbolsPopover : public gui::SymbolPopoverView
 {
     gui::Symbol _pointer;
-    gui::Symbol _rect;
-    gui::Symbol _roundedRect;
-    gui::Symbol _circ;
+    gui::Symbol _wire;
+    gui::Symbol _resistor;
+    gui::Symbol _capacitor;
+    gui::Symbol _inductor;
 
 public:
     SymbolsPopover()
-        : SymbolPopoverView(gui::PopoverView::Type::SymbolOnly, 4, 4), _pointer(":pointer"), _rect(":rect"), _roundedRect(":roundRct"), _circ(":circ")
-    //    , _img(":imgSave")
-    //, _toolbarItemImage(":poly128")
+        : SymbolPopoverView(gui::PopoverView::Type::SymbolOnly, 4, 4), _pointer(":pointer"), _wire(":wire"), _resistor(":resistor"), _capacitor(":capacitor"), _inductor(":inductor")
     {
         gui::Symbol *nullSymbol = nullptr;
         addItem(&_pointer, nullSymbol, tr("pointer"), tr("pointerTT"), false, 0);
-        addItem(&_rect, nullSymbol, tr("rect"), tr("rectTT"), true, 0);
-        addItem(&_roundedRect, nullSymbol, tr("rndRct"), tr("rndRctTT"), true, 0);
-        addItem(&_circ, nullSymbol, tr("circ"), tr("circTT"), true, 0);
+        addItem(&_wire, nullSymbol, tr("rndRct"), tr("rndRctTT"), true, 0);
+        addItem(&_resistor, nullSymbol, tr("rndRct"), tr("rndRctTT"), true, 0);
+        addItem(&_capacitor, nullSymbol, tr("rndRct"), tr("rndRctTT"), true, 0);
+        addItem(&_inductor, nullSymbol, tr("rndRct"), tr("rndRctTT"), true, 0);
         //        setItem(itemPos++, &_img, tr("Save"), tr("SaveTT"), 3.0f, 3.0f);
 
         styleOnParent(false); // remove this line if you want to see color symbols on toolbar
